58eeca8f0c3dbf854cd5c51a4ce29bc8d4d71283
Foster <Tim.Foster@SEPA.org.uk>
2017-11-02 13:41:23 +0000

    docs: correctly test description

--- tests/testthat/test-darleq3-data.R
+++ tests/testthat/test-darleq3-data.R
@@ -8,7 +8,7 @@ test_that("TaxonID and TaxonName should have unique IDs for all rows", {
 })
 
 
-test_that("TaxonID and TaxonName should have unique IDs for all rows", {
+test_that("NBScode should be unique or NA values for each row in table", {
 
   expect_true(all(is.na(darleq3_taxa$NBSCode[duplicated(darleq3_taxa$NBSCode)])))
 
