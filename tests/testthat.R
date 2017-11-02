aa4adaba368f99d477f8dfedfee7e18813530d00
Foster <Tim.Foster@SEPA.org.uk>
2017-11-02 13:26:10 +0000

    test: unique values in darleq3_taxa data

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(darleq3)
+
+test_check("darleq3")
---
+++ tests/testthat/test-darleq3-data.R
@@ -0,0 +1,16 @@
+context("darleq3_taxa")
+
+test_that("TaxonID and TaxonName should have unique IDs for all rows", {
+
+  expect_true(length(darleq3_taxa$TaxonId[duplicated(darleq3_taxa$TaxonId)]) == 0)
+  expect_true(length(darleq3_taxa$TaxonName[duplicated(darleq3_taxa$TaxonName)]) == 0)
+
+})
+
+
+test_that("TaxonID and TaxonName should have unique IDs for all rows", {
+
+  expect_true(all(is.na(darleq3_taxa$NBSCode[duplicated(darleq3_taxa$NBSCode)])))
+
+
+})
